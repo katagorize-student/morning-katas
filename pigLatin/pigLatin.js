784a4f980caa9d298d88b5e84c6ab35502f45ca0
katagorize-student <35737776+katagorize-student@users.noreply.github.com>
2018-01-30 18:19:52 +0000

    Update pigLatin.js

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,9 +1,16 @@
-// Please do not change the name of this function
-var pigLatin = function (str) {
-  if(str === 'sheffield') {
-     return 'effieldshay'
-     }
-  
+function pigLatin (str) {
+let strArray = str.split(' ');
+
+let pigLatinWords = strArray.map(function (word) {
+  let vowelIndex = word.search(/[aeiou]/i);
+  if (/[aeiou]/gi.test(word[0])) return word + 'way';
+  if (/[^aeiou]/gi.test(word[0])) return word.slice(vowelIndex) +  word.slice(0,vowelIndex) + 'ay';
+  return word.slice(1) + word[0] + 'ay';
+});
+
+
+return pigLatinWords.join(' ');
+
 };
   
   if (typeof module !== 'undefined') {
