b08bad52848cf833bbf9da9266361a187ce34cec
katagorize-student <35737776+katagorize-student@users.noreply.github.com>
2018-01-30 18:08:36 +0000

    Update pigLatin.js
    throws error if no string is given.

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,8 +1,7 @@
 // Please do not change the name of this function
 var pigLatin = function (str) {
-  str = 'pigLatin'
-
-  return str;
+  if(str.length === 0) return 'You must enter a valid string.'
+  
 };
   
   if (typeof module !== 'undefined') {
